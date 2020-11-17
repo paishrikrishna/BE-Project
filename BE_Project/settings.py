985d5a9b068764d3d4942615f6536cdb2b92d440
paishrikrishna <2017.shrikrishna.pai@ves.ac.in>
2020-11-17 15:21:35 +0530

    home_page edit 1

--- BE_Project/settings.py
+++ BE_Project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ubd_43n_2s^3%9s$x=ontcoug%tc*ziwzz$yu(@yj@o*@$(ic5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'BE_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
