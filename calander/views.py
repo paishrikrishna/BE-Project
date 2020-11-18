7dadd562aeee8afb28cfdf39e314650c3c9889d8
paishrikrishna <2017.shrikrishna.pai@ves.ac.in>
2020-11-18 16:04:51 +0530

    calandar updated

--- calander/views.py
+++ calander/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def cal_index_page(request):
-	return render(request,"calander.html")
+	if request.method == 'GET':
+		return render(request,"calander.html",{"month":10,"year":2020})
+	else:
+		if request.POST["action"] == "Next" :
+			if int(request.POST['month']) == 12: 
+				return render(request,"calander.html",{"month":1,"year":int(request.POST['year'])+1})
+			elif int(request.POST['month']) < 12:
+				return render(request,"calander.html",{"month":int(request.POST['month'])+1,"year":int(request.POST['year'])})
+		else:
+			if int(request.POST['month']) == 1: 
+				return render(request,"calander.html",{"month":12,"year":int(request.POST['year'])-1})
+			elif int(request.POST['month']) > 1:
+				return render(request,"calander.html",{"month":int(request.POST['month'])-1,"year":int(request.POST['year'])})
