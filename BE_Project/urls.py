d48396a65666f1d10c5170b567872837817f1863
paishrikrishna <2017.shrikrishna.pai@ves.ac.in>
2021-02-07 23:11:21 +0530

    Sensors folder Added

--- BE_Project/urls.py
+++ BE_Project/urls.py
@@ -23,6 +23,7 @@ from login_page.views import login_index_page
 from new_users.views import new_user_index_page
 from notice_board.views import notice_index_page
 from messaging_module.views import message_index_page
+from sensors.views import sensors_index_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,5 +34,6 @@ urlpatterns = [
     path('<str:user>/<str:auth>/requested_events',req_events_index_page),
     path('<str:user>/<str:auth>/notice_board',notice_index_page),
     path('<str:user>/<str:auth>/message',message_index_page),
+    path('<str:user>/<str:auth>/sensors',sensors_index_page),
     path('new_user',new_user_index_page),
 ]
